53eac36a39eeb6cc14c762260fc5086021938abc
lumenstevenarrington <steven.arrington@lumen.com>
2024-03-13 16:49:26 -0600

    fixed battle transitions

--- src/battle_tower.c
+++ src/battle_tower.c
@@ -910,7 +910,7 @@ void StartSpecialBattle(void)
         CreateTask(Task_WaitBT, 1);
         PlayMapChosenOrBattleBGM(0);
         transition = BattleSetup_GetBattleTowerBattleTransition();
-        // BattleTransition_StartOnField(transition);
+        BattleTransition_StartOnField(transition);
         break;
     case 1: // secret base battle
         for (i = 0; i < PARTY_SIZE; i++)
@@ -922,7 +922,7 @@ void StartSpecialBattle(void)
         CreateTask(Task_WaitBT, 1);
         PlayMapChosenOrBattleBGM(0);
         transition = BattleSetup_GetBattleTowerBattleTransition();
-        // BattleTransition_StartOnField(transition);
+        BattleTransition_StartOnField(transition);
         break;
     case 2: // e-reader trainer battle
         ZeroEnemyPartyMons();
@@ -936,7 +936,7 @@ void StartSpecialBattle(void)
         CreateTask(Task_WaitBT, 1);
         PlayMapChosenOrBattleBGM(0);
         transition = BattleSetup_GetBattleTowerBattleTransition();
-        // BattleTransition_StartOnField(transition);
+        BattleTransition_StartOnField(transition);
         break;
     }
 }
